cb3b5e921de215295ce67b6d1bb5d0335209b4ad
Mutuba <danielmutubait@gmail.com>
2024-09-13 13:08:01 +0300

    feat: Finish auth pages and navbar

--- src/shared/NavBar.tsx
+++ src/shared/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect, useRef } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const Navbar: React.FC = () => {
