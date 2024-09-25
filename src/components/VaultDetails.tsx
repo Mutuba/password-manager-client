ece81b87ff305e36acf8845d4d83f5d4767690e5
Mutuba <danielmutubait@gmail.com>
2024-09-25 13:08:44 +0300

    chore: Add a way to go back home from vault details page

--- src/components/VaultDetails.tsx
+++ src/components/VaultDetails.tsx
@@ -130,14 +130,25 @@ const VaultDetails: React.FC = () => {
             />
           )}
           {loading && <Spinner />}
-          {!isVaultOpen && (
-            <button
-              className="bg-green-600 text-white px-4 py-2 rounded-lg shadow-md hover:bg-green-700 transition-colors duration-200 mx-auto block text-center"
-              onClick={handleVaultAccess}
-            >
-              Access Vault
-            </button>
-          )}
+          <div className="mt-6 flex justify-center space-x-4">
+            {!isVaultOpen && (
+              <>
+                <button
+                  className="bg-gray-400 text-white px-4 py-2 rounded-lg shadow-md hover:bg-gray-500 transition-colors duration-200"
+                  onClick={() => navigate("/")}
+                >
+                  Cancel
+                </button>
+
+                <button
+                  className="bg-green-600 text-white px-4 py-2 rounded-lg shadow-md hover:bg-green-700 transition-colors duration-200"
+                  onClick={handleVaultAccess}
+                >
+                  Access Vault
+                </button>
+              </>
+            )}
+          </div>
         </div>
 
         {errors.length > 0 && (
